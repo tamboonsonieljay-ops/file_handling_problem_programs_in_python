5c18480adf491f10d6b9e1aac4ea434af17ba05d
Soniel <tamboonsonieljay@gmail.com>
2026-04-30 00:24:20 +0800

    added a line that writes the even_numbers.txt and odd_numbers.txt

--- numbers_even_odd_filetext.py
+++ numbers_even_odd_filetext.py
@@ -1,5 +1,12 @@
 file = open("numbers.txt", "r")
 
-for line in file:
-    print(line)
-file.close()
+with open ("even_numbers.txt", "w") as file_even, open("odd_numbers.txt", "w") as file_odd:
+
+
+    for num in file:
+        if num % 2 == 0:
+            file_even.write
+        else:
+            file_odd.write
+
+print(num)
