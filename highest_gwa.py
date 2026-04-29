dfd55339e24b494a71e633b810f17197c7df761a
Soniel <tamboonsonieljay@gmail.com>
2026-04-30 02:04:00 +0800

    the student.txt is read as file whereas the name and gwa is separated and the gwa is a float variable

--- highest_gwa.py
+++ highest_gwa.py
@@ -1,2 +1,7 @@
 highest_name = ""
 highest_gwa = float('inf')
+
+with open("top_students_gwa.txt", "r") as file:
+    for line in file:
+        name, gwa = line.strip().split(",")
+        gwa = float(gwa)
