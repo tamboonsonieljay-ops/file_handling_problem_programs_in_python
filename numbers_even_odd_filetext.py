2e24f8b2480c0923c5c4e053c4d434684f8d1c5e
Soniel <tamboonsonieljay@gmail.com>
2026-04-30 00:46:17 +0800

    added a few lines more experimental line such 'int' to make the numbers into integer and also inserted 'with' and 'as' in the first line to make it easier to read

--- numbers_even_odd_filetext.py
+++ numbers_even_odd_filetext.py
@@ -1,12 +1,15 @@
-file = open("numbers.txt", "r")
+with open("numbers.txt", "r") as file:
+    file_numbers = file.read()
+
+file_numbers = [int() for num in file_numbers]
 
 with open ("even_numbers.txt", "w") as file_even, open("odd_numbers.txt", "w") as file_odd:
 
 
-    for num in file:
+    for num in file_numbers:
         if num % 2 == 0:
-            file_even.write
+            file_even.write(str(num))
         else:
-            file_odd.write
+            file_odd.write(str(num))
 
-print(num)
+print("The number.txt file have been sorted into even_numbers.txt and odd_numbers.txt files.")
