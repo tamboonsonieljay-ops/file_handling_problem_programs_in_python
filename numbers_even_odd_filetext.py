7198b92551f0d99eb05d8608b2f9cf18d6b929b3
Soniel <tamboonsonieljay@gmail.com>
2026-04-30 00:55:53 +0800

    fixed all of the code issue that causes the error by adding the '.split()' on the 2nd line and inserting '\n' to lines 11 and 12

--- numbers_even_odd_filetext.py
+++ numbers_even_odd_filetext.py
@@ -1,15 +1,15 @@
 with open("numbers.txt", "r") as file:
-    file_numbers = file.read()
+    file_numbers = file.read().split()
 
-file_numbers = [int() for num in file_numbers]
+file_numbers = [int(num) for num in file_numbers]
 
 with open ("even_numbers.txt", "w") as file_even, open("odd_numbers.txt", "w") as file_odd:
 
 
     for num in file_numbers:
         if num % 2 == 0:
-            file_even.write(str(num))
+            file_even.write(str(num) + "\n")
         else:
-            file_odd.write(str(num))
+            file_odd.write(str(num) + "\n")
 
 print("The number.txt file have been sorted into even_numbers.txt and odd_numbers.txt files.")
