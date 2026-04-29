60c390ba7e47f5fcb32adc32e2c911086e6750c2
Soniel <tamboonsonieljay@gmail.com>
2026-04-30 02:12:25 +0800

    added the condition if gwa < highest_gwa and some variable conversion

--- highest_gwa.py
+++ highest_gwa.py
@@ -5,3 +5,7 @@ with open("top_students_gwa.txt", "r") as file:
     for line in file:
         name, gwa = line.strip().split(",")
         gwa = float(gwa)
+
+        if gwa < highest_gwa: 
+            highest_gwa = gwa
+            highest_name = name
